f2114c0cdb72dee6dd4984fb913a26493cff84b4
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-04-21 10:24:39 +0200

    Redirect to login if token not valid after login

--- backend/main.py
+++ backend/main.py
@@ -64,8 +64,19 @@ async def response(req: Request):
 def frontend(req: Request, path: str):
     path = path.replace('index.html', '')
     d = '../frontend/dist'
+    token = req.cookies.get('token')
+    if token and token.strip() != '':
+        try:
+            openai.api_key = token
+            openai.models.list()
+        except:
+            res = RedirectResponse('/login')
+            res.delete_cookie('token')
+            return res
     if path == 'favicon.ico':
         return FileResponse(f'{d}/public/favicon.ico')
+    if path != 'login' and isdir(f'{d}/{path}') and not req.cookies.get('token'):
+        return RedirectResponse('/login')
     if path == 'login' and req.cookies.get('token'):
         return RedirectResponse('/')
     if path != 'login' and isdir(f'{d}/{path}') and not req.cookies.get('token'):
